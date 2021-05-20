139e5dd030f25d0b4a7efa779d07f11da9a1f87d
@Sant1ago <gustavosantiago1018@gmail.com>
2021-05-20 00:58:27 -0300

    mais ajustes no baseUrl

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {REACT_APP_API_URL} from '@env';
 const api = axios.create({
-    baseURL:`http://192.168.0.105:3335`  
+    baseURL:`${REACT_APP_API_URL}`  
 });
-export default api; 
+export default api;
